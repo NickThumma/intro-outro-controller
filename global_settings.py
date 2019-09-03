f72d3ebc23fa504fc03275fcaab4602ffb73c3b5
Nicole <nickcole.ndt@gmail.com>
2019-09-03 17:34:20 -0400

    routine update

--- global_settings.py
+++ global_settings.py
@@ -2,7 +2,7 @@ from GA_calendar import school_calendar, empty_calendar
 
 class SETTINGS:
 	SESSIONS_PER_WEEK = 3
-	SESSIONS_PER_CHAT = 3
+	SESSIONS_PER_CHAT = 3 #number of sessions in-between chats
 	MAX_SESSIONS = 45 #currently assuming  sessions
 	SCHOOL_CALENDAR = empty_calendar
 	ANIM_FILENAME = "intro_outro_anim.json"
